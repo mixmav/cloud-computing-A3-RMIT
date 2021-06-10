c600d229123d51d3e03b4ce8aa952885336433bd
Manav <manav.sg1@gmail.com>
2021-06-11 01:10:12 +1000

    devv

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,12 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+
+use Ellaisys\Cognito\Auth\RegistersUsers;
+use Ellaisys\Cognito\Auth\VerifiesEmails;
+
 
 class AuthController extends Controller
 {
-	public function __construct() {
-		$this->middleware('guest', ['except' => ['Logout']]);
-	}
+	use RegistersUsers;
+	use VerifiesEmails;
+
+	// public function __construct() {
+	// 	$this->middleware('guest', ['except' => ['Logout']]);
+	// }
 
 	public function LoginPage(){
 		return view('auth.login');
@@ -22,12 +30,16 @@ class AuthController extends Controller
 
 	}
 
-	public function RegisterUser(){
+	public function RegisterUser(Request $request){
 		
+		$collection = collect($request->all());
+        $data = $collection->only('name', 'email', 'password');
 
-		
+		if ($cognitoRegistered=$this->createCognitoUser($data)) {
 
-
+			//If successful, create the user in local db
+			User::create($collection->only('name', 'email'));
+		} //End if
 	}
 
 	public function Logout(){
